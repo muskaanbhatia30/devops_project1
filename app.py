9c2ea4022f4bca2cfc7c5cfb35a1f016e03a8611
muskaanbhatia30 <2019pietcsmuskaan103@poornima.org>
2025-07-06 18:46:48 +0530

    app.py adding

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/info")
+def lwinfo():
+ return "i m LW from India"
+
+@app.route("/info")
+def lwphone() :
+ return "9351009002"
+
+
+app.run(host="0.0.0.0")
